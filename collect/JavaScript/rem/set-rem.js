2bfc11cf83ab3cc2cf8ca3cfa2e34cc9d98af393
zenghongcong <zenghongcong@clickwifi.net>
2017-04-10 15:00:18 +0800

    rem font-size 设置优化

--- collect/JavaScript/rem/set-rem.js
+++ collect/JavaScript/rem/set-rem.js
@@ -4,20 +4,18 @@
 * @Last Modified by:   anchen
 * @Last Modified time: 2016-11-25 17:37:20
 */
-function adapt(designWidth, rem2px){
-  var d = window.document.createElement('div');
-  d.style.width = '1rem';
-  d.style.display = "none";
-  var head = window.document.getElementsByTagName('head')[0];
-  head.appendChild(d);
-  var defaultFontSize = parseFloat(window.getComputedStyle(d, null).getPropertyValue('width'));
-  d.remove();
-  document.documentElement.style.fontSize = window.innerWidth / designWidth * rem2px / defaultFontSize * 100 + '%';
-  var st = document.createElement('style');
-  var portrait = "@media screen and (min-width: "+window.innerWidth+"px) {html{font-size:"+ ((window.innerWidth/(designWidth/rem2px)/defaultFontSize)*100) +"%;}}";
-  var landscape = "@media screen and (min-width: "+window.innerHeight+"px) {html{font-size:"+ ((window.innerHeight/(designWidth/rem2px)/defaultFontSize)*100) +"%;}}"
-  st.innerHTML = portrait + landscape;
-  head.appendChild(st);
-  return defaultFontSize
-};
-var defaultFontSize = adapt(640, 100);
+(function (doc, win) {
+		var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function () {
+				var clientWidth = docEl.clientWidth,
+						clientHeight = docEl.clientHeight,
+						designWidth = 640,
+						designHeight = 1136;
+				if (!clientWidth) return;
+				docEl.style.fontSize = 100 * (clientWidth / 640) * clientHeight / (designHeight * clientWidth / designWidth)  + 'px';
+		};
+		if (!doc.addEventListener) return;
+		win.addEventListener(resizeEvt, recalc, false);
+		doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
